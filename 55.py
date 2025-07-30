d49d4348cf2bbd14bee30375e2b2e733de1fc642
cristian alejandro perez <mandymoorejcpaz@gmail.com>
2025-07-30 01:25:05 +0000

    56 despues sigo

---
+++ 55.py
@@ -0,0 +1,7 @@
+"""55.  Hacer un algoritmo que realice la sumatoria de los números enteros comprendidos entre 
+el 1 y el 10, lo que se pide es: 1+2+3+....+10"""
+suma=0
+for  i in range(1,11):
+    suma+=i
+    
+print(f"la suma es {suma}")
---
+++ 56.py
@@ -0,0 +1,4 @@
+"""56.  Hacer un algoritmo que realice la sumatoria de los números enteros múltiplos de 5, 
+comprendidos entre el 1 y el 100, lo que se pide es, 5 + 10 + 15 +.... + 100. El programa 
+deberá mostrar por pantalla los números en cuestión con un mensaje y finalmente su 
+sumatoria"""
